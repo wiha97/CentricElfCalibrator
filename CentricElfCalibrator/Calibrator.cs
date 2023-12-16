7e3379af2e6077dd06a5181eb22847aa61031816
wiha97 <wiha97@protonmail.com>
2023-12-16 17:19:04 +0100

    Init

---
+++ CentricElfCalibrator/Calibrator.cs
@@ -0,0 +1,32 @@
+namespace CentricElfCalibrator;
+
+public class Calibrator
+{
+    public static int Total { get; set; }
+
+    public static List<string> GetCalibration(string file)
+    {
+        string[] lines = File.ReadAllLines(file.Trim());
+        List<string> newLines = new();
+        foreach (string line in lines)
+        {
+            int i = GetNum(line);
+            Total += i;
+            newLines.Add($"{i} - {line}");
+        }
+        return newLines;
+    }
+
+    private static int GetNum(string word)
+    {
+        int num = 0;
+
+        string nums = string.Concat(word.Where(Char.IsDigit));
+        char first = nums[0];
+        char last = nums[nums.Length - 1];
+
+        num = int.Parse($"{first}{last}");
+        
+        return num;
+    }
+}
---
+++ CentricElfCalibrator/Program.cs
@@ -0,0 +1,14 @@
+﻿using CentricElfCalibrator;
+
+string file = "The_Elves_List.txt";
+if (!File.Exists(file))
+{
+    Console.WriteLine("Please enter path to file:");
+    file = Console.ReadLine();
+}
+
+foreach (string line in Calibrator.GetCalibration(file))
+    Console.WriteLine(line);
+
+Console.WriteLine($"\nCalibration number is: {Calibrator.Total}\n\nPress any key to exit...");
+Console.ReadKey();
